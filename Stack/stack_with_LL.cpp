a62c3ce9bbda061d05743d1e631e44355efbb5f3
Somasekhar <s.sekharofficial@gamil.com>
2021-08-10 07:23:02 +0530

    Added push(data) Func

--- Stack/stack_with_LL.cpp
+++ Stack/stack_with_LL.cpp
@@ -6,12 +6,25 @@ struct Node{
     Node *next;
 } *top = NULL;
 
-void create_Node(int data){
+Node* create_Node(int data){
     Node *newnode = new Node;
     newnode-> data = data;
     newnode->next = NULL;
+    return newnode;
 }
 
+void push(int data){
+    if(top == NULL){
+        top == create_Node(data);
+    }
+    else{
+        Node *temp;
+        temp = top;
+        top = create_Node(data);
+        top->next  = temp;
+    }
+
+} 
 
 int main(){
 
